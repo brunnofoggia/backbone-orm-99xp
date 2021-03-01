124b627383e07ce885f5d7952b47040312247211
Bruno Foggia <bruno.foggia@cbyk.com.br>
2021-03-01 16:41:29 -0300

    update on deps

--- src/backbone-orm-99xp.js
+++ src/backbone-orm-99xp.js
@@ -1,7 +1,6 @@
-//Backbone ORM is result of fusion between [backbone](https://backbonejs.org) and 
+//Backbone ORM is result of fusion between [backbone](https://backbonejs.org) and
 //[sequelize](https://sequelize.org/) to provide a set of functionalities for business models integrated to ORM.
 // It includes an integration to provide full featured validations with [validate-99xp](https://github.com/brunnofoggia/validate-99xp)
-
 
 // Instructions
 // --------------
@@ -45,25 +44,25 @@
 
 // Baseline setup
 // --------------
-import _ from 'underscore-99xp';
-import Backbone from 'backbone';
-import bbx from 'backbone-99xp';
-import v from 'validate-99xp';
-import AppException from 'app-exception';
+import _ from "underscore-99xp";
+import Backbone from "backbone";
+import bbx from "backbone-99xp";
+import v from "validate-99xp";
+import AppException from "app-exception";
 
 var BackboneORM = {};
 
 var extended = {
-    _migration: {alter: true},
+    _migration: { alter: true },
     migration() {
-        var canMigrate = _.result(this, 'canMigrate');
+        var canMigrate = _.result(this, "canMigrate");
         if (!canMigrate) {
             return false;
         }
         return this._migration;
     },
     syncMigration() {
-        var migration = _.result(this, 'migration');
+        var migration = _.result(this, "migration");
         if (!migration) {
             return;
         }
@@ -76,11 +75,15 @@ var extended = {
             conn = this.getConnection();
         switch (true) {
             case _.isArray(definition):
-                o = conn.isDefined(definition[0]) ?
-                    conn.model(definition[0]) :
-                    conn.define(definition[0], definition[1] || {}, definition[2] || {});
+                o = conn.isDefined(definition[0])
+                    ? conn.model(definition[0])
+                    : conn.define(
+                          definition[0],
+                          definition[1] || {},
+                          definition[2] || {}
+                      );
                 break;
-            case typeof definition === 'function':
+            case typeof definition === "function":
                 o = definition.sequelize ? definition : definition();
                 break;
         }
@@ -100,24 +103,30 @@ var extendedModel = {
             return this.entity;
         }
 
-        var o = _.result(this, 'entityDefinition') || (BackboneORM.error('Entity Definition not found'));
+        var o =
+            _.result(this, "entityDefinition") ||
+            BackboneORM.error("Entity Definition not found");
         this.entity = this.defineEntity(o);
 
         return this.entity;
     },
     // Retrives connection object from this.conn or BackboneORM.conn
     getConnection() {
-        return _.result(this, 'conn') || _.result(BackboneORM, 'conn') || (BackboneORM.error('Database connection not set'));
+        return (
+            _.result(this, "conn") ||
+            _.result(BackboneORM, "conn") ||
+            BackboneORM.error("Database connection not set")
+        );
     },
     // Customization that redirect calls accordingly to the method asked (read, create, update, patch, delete)
     sync(method, model, o) {
-        model.trigger('request', method, model, o);
-        return this['sync' + _.capitalize(method)](method, model, o);
+        model.trigger("request", method, model, o);
+        return this["sync" + _.capitalize(method)](method, model, o);
     },
     // Get row
     syncRead(method, model, o) {
         // callbacks for success or error. They trigger Backbone default ones
-        var success = r => {
+        var success = (r) => {
             o.success(r.dataValues);
         };
 
@@ -135,12 +144,15 @@ var extendedModel = {
         }
 
         // run select
-        return this.entity.findOne({
-            where: data
-        }).then(r => {
-            this.formatMe();
-            success(r);
-        }).catch(_.bind(this.handleSyncError, this));
+        return this.entity
+            .findOne({
+                where: data,
+            })
+            .then((r) => {
+                this.formatMe();
+                success(r);
+            })
+            .catch(_.bind(this.handleSyncError, this));
     },
     // Insert row
     syncCreate(method, model, o) {
@@ -152,10 +164,13 @@ var extendedModel = {
         };
 
         // remove pk from attributes that will be updated
-        var attrs = this.unformat(_.omit(_.clone(this.attributes), this.idAttribute));
+        var attrs = this.unformat(
+            _.omit(_.clone(this.attributes), this.idAttribute)
+        );
         // run insert
-        return this.entity.create(attrs)
-            .then(r => success(r))
+        return this.entity
+            .create(attrs)
+            .then((r) => success(r))
             .catch(_.bind(this.handleSyncError, this));
     },
     // Update row
@@ -166,14 +181,17 @@ var extendedModel = {
         };
 
         // remove pk from attributes that will be updated
-        var attrs = this.unformat(_.omit(_.clone(this.attributes), this.idAttribute));
+        var attrs = this.unformat(
+            _.omit(_.clone(this.attributes), this.idAttribute)
+        );
         // build where
         var _o = {
-            where: _.pick(this.attributes, this.idAttribute)
+            where: _.pick(this.attributes, this.idAttribute),
         };
         // run update
-        return this.entity.update(attrs, _o)
-            .then(r => success(r))
+        return this.entity
+            .update(attrs, _o)
+            .then((r) => success(r))
             .catch(_.bind(this.handleSyncError, this));
     },
     // Patch row
@@ -191,7 +209,7 @@ var extendedModel = {
         } else {
             errors.push(e.message || e);
         }
-        this.trigger('error', e, errors);
+        this.trigger("error", e, errors);
     },
     // Validations list. See [validate-99xp](https://github.com/brunnofoggia/validate-99xp)
     validations(attrs, options) {
@@ -199,32 +217,52 @@ var extendedModel = {
     },
     // Dispatcher of validation errors
     validationErrors(err) {
-        return BackboneORM.error({
-            title: 'Invalid Data',
-            errors: err
-        }, 0, 400);
+        return BackboneORM.error(
+            {
+                title: "Invalid Data",
+                errors: err,
+            },
+            0,
+            400
+        );
     },
     // Allows to set a pair of listeners where one turns the other off after being executed.
     // Both parameters are arrays composed like [event, callback]
     once(c1, c2) {
         if (_.isArray(c1) && _.isArray(c2)) {
-            this.once(c1[0], _.partial(function (c1, c2) {
-                this.off(c2[0], c2[1]);
+            this.once(
+                c1[0],
+                _.partial(
+                    function (c1, c2) {
+                        this.off(c2[0], c2[1]);
 
-                var args = _.toArray(arguments);
-                args.shift();args.shift();
+                        var args = _.toArray(arguments);
+                        args.shift();
+                        args.shift();
 
-                c1[1].apply(null, args);
-            }, c1, c2));
+                        c1[1].apply(null, args);
+                    },
+                    c1,
+                    c2
+                )
+            );
 
-            this.once(c2[0], _.partial(function (c1, c2) {
-                this.off(c1[0], c1[1]);
+            this.once(
+                c2[0],
+                _.partial(
+                    function (c1, c2) {
+                        this.off(c1[0], c1[1]);
 
-                var args = _.toArray(arguments);
-                args.shift();args.shift();
+                        var args = _.toArray(arguments);
+                        args.shift();
+                        args.shift();
 
-                c2[1].apply(null, args);
-            }, c1, c2));
+                        c2[1].apply(null, args);
+                    },
+                    c1,
+                    c2
+                )
+            );
         }
 
         return _.bind(Backbone.Model.prototype.once, this)(c1, c2);
@@ -233,13 +271,13 @@ var extendedModel = {
         return new Promise((resolve, reject) => {
             this.once(
                 [
-                    'sync',
+                    "sync",
                     () => {
                         resolve(this);
                     },
                 ],
                 [
-                    'error',
+                    "error",
                     (err) => {
                         reject(err);
                     },
@@ -248,11 +286,13 @@ var extendedModel = {
 
             _.bind(bbx.model.prototype.save, this)();
         });
-    }
+    },
 };
 
 // Extension of Backbone.Model added to custom behaviors
-BackboneORM.Model = bbx.model.extend(_.extend(_.clone(v), extended, extendedModel));
+BackboneORM.Model = bbx.model.extend(
+    _.extend(_.clone(v), extended, extendedModel)
+);
 
 var extendedCollection = {
     // Pré set entity into collection instance
@@ -261,8 +301,8 @@ var extendedCollection = {
         this.setEntity();
     },
     setModelBase() {
-        var ModelBase = (this.model ? this.model : BackboneORM.Model.extend());
-        this.modelBase = new ModelBase;
+        var ModelBase = this.model ? this.model : BackboneORM.Model.extend();
+        this.modelBase = new ModelBase();
     },
     // Load an instance of given class
     setEntity() {
@@ -270,53 +310,68 @@ var extendedCollection = {
             return this.entity;
         }
 
-        var o = _.result(this, 'entityDefinition') || _.result(this.modelBase, 'entityDefinition') || BackboneORM.error('Entity Definition not found');
+        var o =
+            _.result(this, "entityDefinition") ||
+            _.result(this.modelBase, "entityDefinition") ||
+            BackboneORM.error("Entity Definition not found");
         this.entity = this.defineEntity(o);
 
         return this.entity;
     },
     // Retrives connection object from this.conn or BackboneORM.conn
     getConnection() {
-        return _.result(this, 'conn') || _.result(this.modelBase, 'conn') || _.result(BackboneORM, 'conn') || BackboneORM.error('Database connection not set');
+        return (
+            _.result(this, "conn") ||
+            _.result(this.modelBase, "conn") ||
+            _.result(BackboneORM, "conn") ||
+            BackboneORM.error("Database connection not set")
+        );
     },
     // Transform sequelize rows into models
     parse(r) {
         if (_.isArray(r) && _.size(r) > 0) {
-            this.add(_.map(r, (row) => {
-                return (row.dataValues);
-            }));
+            this.add(
+                _.map(r, (row) => {
+                    return row.dataValues;
+                })
+            );
         }
     },
     // find all records accordingly to the conditions
     findAll(where = {}) {
         return new Promise((resolve, reject) => {
-            return this.entity.findAll({
-                where: where
-            })
-            .then((r) => {
-                this.parse(r);
-                this.formatModels();
-                resolve(this);
-            })
-            .catch((err) => {
-                reject(err);
-            });
+            return this.entity
+                .findAll({
+                    where: where,
+                })
+                .then((r) => {
+                    this.parse(r);
+                    this.formatModels();
+                    resolve(this);
+                })
+                .catch((err) => {
+                    reject(err);
+                });
         });
     },
     // Save all models
     saveAll() {
         return new Promise((resolve, reject) => {
             var size = this.models.length,
-            c = () => {
-                if (--size > 0) { return; }
-                // resolve after save all models
-                resolve(this);
-            };
+                c = () => {
+                    if (--size > 0) {
+                        return;
+                    }
+                    // resolve after save all models
+                    resolve(this);
+                };
 
             // validate all models first
             var errors = [];
             for (let x in this.models) {
-                var validate = this.models[x].validate(this.model.attributes, {validateAll: true});
+                var validate = this.models[x].validate(this.model.attributes, {
+                    validateAll: true,
+                });
                 if (validate) {
                     errors = errors.concat(validate);
                 }
@@ -328,16 +383,19 @@ var extendedCollection = {
             // save each model
             for (let x in this.models) {
                 var m = this.models[x];
-                m.save().then(()=>c()).catch((err)=>{
-                    reject(err);
-                });
+                m.save()
+                    .then(() => c())
+                    .catch((err) => {
+                        reject(err);
+                    });
             }
         });
-    }
+    },
 };
 
-BackboneORM.Collection = bbx.collection.extend(_.extend({}, extended, extendedCollection));
-
+BackboneORM.Collection = bbx.collection.extend(
+    _.extend({}, extended, extendedCollection)
+);
 
 BackboneORM.error = function (msg, code = 0, status = 500) {
     throw new AppException(msg, code, status);
